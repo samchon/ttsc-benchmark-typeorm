fb56c549ec0537a7631c58b57ba3df22f8f2efd9
samchon <samchon.github@gmail.com>
2026-05-22 19:45:41 +0900

    fix: narrow ttsc lint benchmark rules

--- lint.config.ts
+++ lint.config.ts
@@ -13,15 +13,5 @@ export default {
   rules: {
     "no-var": "error",
     "prefer-const": "error",
-    eqeqeq: "error",
-    "object-shorthand": "error",
-    "no-unneeded-ternary": "error",
-    "prefer-template": "error",
-    "no-useless-rename": "error",
-    "dot-notation": "error",
-    "no-extra-boolean-cast": "error",
-    "no-useless-escape": "error",
-    "prefer-as-const": "error",
-    "prefer-namespace-keyword": "error",
   },
 } satisfies ITtscLintConfig;
