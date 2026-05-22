1319cea82884b2db4a8016a3f9bb16c3e9b6798b
samchon <samchon.github@gmail.com>
2026-05-22 19:43:08 +0900

    fix: remove unused yargs type imports

--- src/commands/InitCommand.ts
+++ src/commands/InitCommand.ts
@@ -1,6 +1,5 @@
 import ansi from "ansis"
 import path from "node:path"
-import type yargs from "yargs"
 import { TypeORMError } from "../error"
 import { PlatformTools } from "../platform/PlatformTools"
 import { CommandUtils } from "./CommandUtils"
